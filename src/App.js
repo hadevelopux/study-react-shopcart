9afe31876bdd1ac07cc926e808a2db731dd7d9bb
harryalejandro <harryalejandro@gmail.com>
2024-05-16 14:36:32 +0000

    Estado inicial y cargando imagenes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Component } from "react";
 
 class App extends Component {
+    state = {
+        productos: [
+            { name: 'ELDEN RING', price: 60, img: '/productos/eldenring.jpg' },
+            { name: 'Hades II', price: 30, img: '/productos/hades2.jpg' },
+            { name: 'HELLDIVERS™ 2', price: 40, img: '/productos/helldivers2.jpg' },
+        ]
+    }
     render() {
         return (
             <div>
